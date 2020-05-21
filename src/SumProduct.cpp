461162c5b371a994327d64db90eac670e70c1240
Olin Feist <feisto@wwu.edu>
2020-05-21 16:46:58 -0700

    Added user input to take file name

--- src/SumProduct.cpp
+++ src/SumProduct.cpp
@@ -6,11 +6,20 @@
 #include <iomanip>
 using namespace std;
 
-//import csv file to "file"
-ifstream file("5km_grid_combined_with_0to500dm_ngdc_vat.csv");
+
 
 
 int main(){
+
+    //code takes user input for file name
+    inputusrfile:
+    string filename;
+    cout << "Type your csv file name(name.csv): ";
+    cin >> filename; // get user input from the keyboard
+    cout << "Your File is: " << filename<<endl;
+
+    //import csv file to "file"
+    ifstream file(filename.c_str());
 
     //outputdata is an array of vectors used to represent a table with GridID | SumCount | Sum(Count*depth)
     vector <int> outputdata[3];
@@ -19,6 +28,14 @@ int main(){
     string line;
     getline(file, line);
 
+    //test if file was loaded
+    if(line.empty()){
+        cout<<"file not loaded"<<endl;
+        goto inputusrfile;
+    }else{
+        cout<<"File Loaded..."<<endl;
+        cout<<"Running..."<<endl;
+    }
 
 
     //vector V is use to hold current line of csv file
@@ -90,6 +107,6 @@ int main(){
 
     //close file
    file_stream.close();
-    
+    cout<<"Done."<<endl;
     return 0;
 }
